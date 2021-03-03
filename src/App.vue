ffb32c68dd7bbbf54d560004da26f4cfbde6192f
raghavsharma007 <princeraghav.sharma@gmail.com>
2021-03-03 23:03:08 +0530

    Ninja game components added

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="text-3xl header-title">Ninjustu Finish Timer</h1>
+  <button @click="start" :disabled="isPlaying" class="bg-gray-300 focus:outline-none">play</button>
+  <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Block },
+  data(){
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start() {
+        this.delay = 2000 + Math.random() * 5000;
+        this.isPlaying = true;
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=RocknRoll+One&display=swap');
+
+.header-title {
+  font-family: 'RocknRoll One', sans-serif;
 }
+
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Click Me {{delay}}
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['delay']
+
+}
+</script>
+
+<style>
+
+</style>
